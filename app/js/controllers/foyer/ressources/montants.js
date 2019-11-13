df58d4318b4732d0bed30f992b2e871b814d4e76
Herizo Ludovic <herizo.ludovic@gmail.com>
2019-11-13 12:45:21 +0300

    premier test directement vers montant salaire

--- app/js/controllers/foyer/ressources/montants.js
+++ app/js/controllers/foyer/ressources/montants.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('ddsApp').controller('FoyerRessourcesMontantsCtrl', function($scope, $stateParams, ressourceTypes, RessourceService, IndividuService) {
-    console.log(ressourceTypes);
-    console.log($scope.selectedRessourceTypes);
+   
+    //console.log($scope.selectedRessourceTypes); //{salaire_net: true}
+
+
+    $scope.selectedRessourceTypes = {salaire_net:true};
+    
     $scope.yearMoins1 = moment($scope.situation.dateDeValeur).subtract(1, 'years').format('YYYY');
     $scope.currentMonth = moment($scope.situation.dateDeValeur).format('MMMM YYYY');
 
